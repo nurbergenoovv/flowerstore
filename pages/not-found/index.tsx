3f60d823b2616e83059d690a309351c86fd7ff60
Ибрахим Нурберген <ibrahimnurbergen@Ibrahims-MacBook-Air.local>
2024-09-23 13:14:01 +0500

    updated not found page

--- pages/not-found/index.tsx
+++ pages/not-found/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
 
 export default function NotFound() {
   return (
-    <div>
-      <h1>Not found page</h1>
+    <div className="w-screen h-screen flex flex-col justify-center items-center font-bold uppercase bg-gradient-to-b from-yellow-200 to-green-200 cursor-pointer text-white">
+      <h1 className="text-7xl hover:text-black duration-500">404</h1>
+      <p className="text-center hover:text-black duration-500">Not found page</p>
     </div>
   )
 }
